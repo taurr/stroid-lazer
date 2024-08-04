ee8d02f75f255abe5a127360c93f287af7400693
Johnny Tidemann Vestergaard <jtv@jrtv.dk>
2024-08-04 20:28:12 +0200

    `auto_refresh.sh` fails executing after `.trigger` was removed from the repository. Fixes #1

--- auto_refresh.sh
+++ auto_refresh.sh
@@ -2,6 +2,7 @@
 
 trap 'kill %1; kill $pid2; exit' SIGINT
 
+touch .trigger
 cargo watch -i .trigger -i .vscode -i .git -i target -i perf.data --postpone -q -x build -s 'touch .trigger' &
 cargo watch -w .trigger -q -c -s 'CARGO_MANIFEST_DIR=/home/johnny/Projects/personal/stroid-lazer/ RUST_LOG=info,wgpu=warn,stroid_lazer=debug target/debug/stroid-lazer ' &
 pid2=$!
